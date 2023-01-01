b8f1a6368c56eb891f328c3b5ced972ef8e6ff97
Willian G <willflorgasp@gmail.com>
2023-01-01 15:53:36 -0300

    Operadores finalizados e metodos iniciados

---
+++ TiposVariaveis/src/TiposdeOperadores.java
@@ -0,0 +1,8 @@
+public class TiposdeOperadores {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+/* float ou doble Representam Quase a mesma coisa, porem é mais comum se usar doble 
+ * 
+*/
